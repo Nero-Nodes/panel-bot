caf4236ead72ca946dd0ee701e389096d1875af8
Cam White (Jex) <72230943+cmrxnn@users.noreply.github.com>
2022-02-20 01:01:58 +0000

    create basic working system

--- client/index.js
+++ client/index.js
@@ -5,3 +5,20 @@
 // Copyright (c) neronodes.net
 // Licensed under the MIT license.
 // -------------------------------
+
+// Require the necessary dependencies for this project.
+const { Client, Intents } = require('discord.js');
+
+// Import configurations we'll need.
+const { token } = require('../config/discord.json');
+
+// Create a new Discord client instance.
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+// Login to Discord with your client's token
+client.login(token);
+
+// When the client is ready, log it to the console.
+client.once('ready', () => {
+	console.log('success: client connected to API');
+});
