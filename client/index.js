2969e7115b8bdb445db7d268010177009d3cedb3
Cam White (Jex) <72230943+cmrxnn@users.noreply.github.com>
2022-02-20 00:47:32 +0000

    create initial structure

---
+++ client/index.js
@@ -0,0 +1,7 @@
+// -------------------------------
+// github.com/nero-nodes/panel-bot
+// client/index.js
+//
+// Copyright (c) neronodes.net
+// Licensed under the MIT license.
+// -------------------------------
---
+++ tests/index.js
@@ -0,0 +1,12 @@
+// -------------------------------
+// github.com/nero-nodes/panel-bot
+// tests/index.js
+//
+// Copyright (c) neronodes.net
+// Licensed under the MIT license.
+// -------------------------------
+
+console.warn('warn: tests are not yet available for this application')
+
+console.error('fatal: exiting application with error code 1')
+process.exit(1)
